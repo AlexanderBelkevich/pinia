8dffc629889cea47f7627ca3d513d2119c5c20a7
Alexander Belkevich <a.belkevich@kodland.team>
2022-11-10 11:24:31 +0400

    lesson8

--- src/stores/MovieStore.js
+++ src/stores/MovieStore.js
@@ -1,9 +1,14 @@
 import { defineStore } from "pinia";
-import { ref, computed } from "vue";
+import { ref, computed, watch } from "vue";
 
 export const useMovieStore = defineStore("movieStore", () => {
   const movies = ref([]);
   const activeTab = ref(2);
+
+  const moviesInLocalStorage = localStorage.getItem("movies");
+  if (moviesInLocalStorage) {
+    movies.value = JSON.parse(moviesInLocalStorage)._value;
+  }
 
   const watchedMovies = computed(() =>
     movies.value.filter((el) => el.isWatched)
@@ -20,6 +25,14 @@ export const useMovieStore = defineStore("movieStore", () => {
   const deleteMovie = (id) => {
     movies.value = movies.value.filter((el) => el.id !== id);
   };
+
+  watch(
+    () => movies,
+    (state) => {
+      localStorage.setItem("movies", JSON.stringify(state));
+    },
+    { deep: true }
+  );
 
   return {
     movies,
